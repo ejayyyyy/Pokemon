e5ae3061b5e6fbc8132eeb4eea5833aa70879e58
ejayyyyy <janerikmandocdoc15@gmail.com>
2024-03-17 14:33:55 +0800

    feat: add modal pokemon card

--- src/components/Pokemon/PokemonCard.jsx
+++ src/components/Pokemon/PokemonCard.jsx
@@ -1,10 +1,78 @@
-const PokemonCard = () => {
+import { COLORS_CARD } from "../../lib/constants/PokemonColorCard";
+import PokemonTypes from "./PokemonTypes";
+import { IoClose } from "react-icons/io5";
+
+const PokemonCard = ({ pokemon, toggleCard }) => {
+    const healthBarWidth = pokemon.maxHP > 10000 ? 'full' : `${Math.round((pokemon.maxHP / 10000) * 100)}`;
+    const combatBarWidth = pokemon.maxCP > 10000 ? 'full' : `${Math.round((pokemon.maxCP / 10000) * 100)}`;
+
+    const colorTypes = (type) => {
+        return COLORS_CARD[type] || 'text-gray-600'
+    }
     return (
-        <div
-            className="absolute top-0"
-        >
-            <h1>Hello</h1>
-        </div>
+        <>
+            <div
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[11]"
+            >
+                <div
+                    className="bg-pokemon-white-100 shadow-2xl px-10 py-14 rounded-2xl w-[350px] md:w-[500px] relative"
+                >
+                    <IoClose
+                        className="absolute top-[15px] right-[20px] text-5xl text-pokemon-black-100 font-medium cursor-pointer"
+                        onClick={ toggleCard }
+                    />
+                    <img
+                        src={ pokemon.image }
+                        alt={ pokemon.id }
+                        className="w-[300px] mb-5 md:w-[400px]"
+                    />
+
+                    <div className={`flex flex-col gap-5 ${ colorTypes(pokemon.types[0]) } rounded-2xl p-5 shadow-lg`}>
+                        <div className="flex flex-col gap-3">
+                            <h1
+                                className="text-2xl md:text-4xl"
+                            >
+                                { pokemon.name }
+                            </h1>
+
+                            <PokemonTypes types={ pokemon.types }/>
+                        </div>
+
+                        <div className="flex flex-col gap-5">
+                            <div>
+                                <p
+                                    className="text-md md:text-xl flex justify-between"
+                                >Maximum Health Points:
+                                    <span
+                                        className="font-semibold"
+                                    >{ pokemon.maxHP }</span>
+                                </p>
+                                <div className="bg-white h-3 w-full mb-2">
+                                    <div className={`bg-pokemon-green-200 h-full w-[${ healthBarWidth }%]`}></div>
+                                </div>
+                            </div>
+
+                            <div>
+                                <p
+                                    className="text-md md:text-xl flex justify-between"
+                                >Maximum Combat Points:
+                                    <span
+                                        className="font-semibold"
+                                    >{ pokemon.maxCP }</span>
+                                </p>
+
+                                <div className="bg-white h-3 mb-2">
+                                    <div className={`bg-pokemon-yellow-200 h-full w-[${combatBarWidth}%]`}></div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className="absolute top-0 w-full z-[10] left-0 h-[100vh] bg-black opacity-50"></div>
+        </>
     );
 }
  
--- src/components/Pokemon/PokemonList.jsx
+++ src/components/Pokemon/PokemonList.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import PokemonTypes from "./PokemonTypes";
+import PokemonCard from "./PokemonCard";
 
 const PokemonList = ({ pokemons }) => {
+    const [isCard, setCard] = useState(false)
+    const [pokemon, setPokemon] = useState(null)
+    
+    const toggleCard = (pokemon) => {
+        setCard(!isCard)
+        setPokemon(pokemon)
+    }
+
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-5">
+            { isCard && pokemon && <PokemonCard pokemon={ pokemon } toggleCard={ toggleCard } /> }
             {pokemons.map(pokemon => (
                 <div
-                    className="flex flex-row p-3 gap-5 rounded-2xl justify-between shadow-xl bg-red-100"
+                    className="flex flex-row p-3 gap-5 rounded-2xl justify-between shadow-xl bg-red-100 cursor-pointer"
+                    onClick={ () => toggleCard(pokemon) }
                     key={ pokemon.id }>
                         <div className="flex flex-col gap-2 w-[70%]">
                             <h1
